cc43eae8df4269d0cb350f40b4631a00f5eeaa27
valvalmew <valentinaatamaniuc.r@gmail.com>
2025-02-09 17:47:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function openArticles(){
+
+    window.location.href = "index.html";
+    
+    }
+    
+    function openContacts(){ 
+    
+    window.location.href ="contacts.html";
+    
+    }
+    
+    function openReviews(){
+    
+    window.location.href = "reviews.html";
+    
+    }
+    
